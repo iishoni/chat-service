1d267e49551fa81b9a1b789be85cfde3bd85012f
dxk19911212 <648638730@qq.com>
2018-06-04 18:20:46 +0800

    测试

--- src/main/java/com/iishoni/chatservice/netty/util/JsonUtil.java
+++ src/main/java/com/iishoni/chatservice/netty/util/JsonUtil.java
@@ -1,6 +1,5 @@
 package com.iishoni.chatservice.netty.util;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.iishoni.chatservice.netty.model.Message;
 
